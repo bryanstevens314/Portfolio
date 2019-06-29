8b4c55b56cac96d9b6457116cf716c1dc2dad182
Bryan <bryan_stevens314@yahoo.com>
2019-06-29 15:48:57 -0700

    Initial commit - blank canvas

---
+++ client/components/navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Navbar = () => (
+  <div className="nav">
+  </div>
+)
+export default Navbar;
---
+++ client/routes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { Home } from './components'
+
+class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route component={Home} />
+      </Switch>
+    )
+  }
+}
+export default Routes;
