56c27e9fc3f89fb8a64912ee58e52b9285f55c13
Bryan <bryan_stevens314@yahoo.com>
2019-06-29 19:34:11 -0700

    Feature: Basic layout

--- client/components/home.js
+++ client/components/home.js
@@ -1,7 +1,36 @@
 import React from 'react'
 
 const Home = () => (
-  <div>
+  <div className="home">
+    <Texture />
+    <Detail />
+    <Style />
+    <Leather />
+  </div>
+)
+
+const Texture = () => (
+  <div className="texture">
+    <img src="./texture2.jpg" />
+  </div>
+)
+const Detail = () => (
+  <div className="detail">
+    <div className="wrapper">
+      {/* <img className="des1" src="./<>.png" />
+      <img className="ASE" src="./ASE-CERTIFIED-LOGO.png" />
+      <p>Software Engineer turned Mechanic. I am a Central Coast local constantly striving towards a better life for myself and those around me. Let’s chat.</p> */}
+    </div>
+  </div>
+)
+const Style = () => (
+  <div className="style">
+  </div>
+)
+const Leather = () => (
+  <div className="leather">
+    <div className="leather_Detail" />
+    <img src="./leather.jpg" />
   </div>
 )
 
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const Navbar = () => (
   <div className="nav">
+    <p>|||</p>
+    <img src="./react.png" />
   </div>
 )
 export default Navbar;
