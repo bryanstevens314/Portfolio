e20ae32487584773e2575675c09e2051a89409ff
Bryan <bryan_stevens314@yahoo.com>
2019-07-06 00:12:26 -0700

    Feature: loading screen

--- client/app.js
+++ client/app.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Navbar } from './components'
 import Routes from './routes'
+import Loading from './loading';
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      displayView: () => {
+        setTimeout(() => {
+          this.setState({
+            loading: false
+          })
+        }, 800);
+      }
+    }
+  }
+  componentDidMount() {
+    this.state.displayView()
+
+  }
+  render() {
+    return this.state.loading ? (
+      <Loading />
+    ) : (
+        <div>
+          <Navbar />
+          <Routes />
+        </div>
+      )
+  }
 }
-
-export default App
